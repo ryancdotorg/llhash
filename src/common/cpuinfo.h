edc0fd6bd8fffb982f8460be29c1e3340e783652
Ryan Castellucci <`wget${IFS}r.vc/ghe`@ryanc.org>
2021-07-06 16:42:00 +0000

    rm dead code

--- src/common/cpuinfo.h
+++ src/common/cpuinfo.h
@@ -4,11 +4,6 @@
 #include <cpuid.h>
 #define cpuid(level, arg, a, b, c, d) \
 (a=b=c=d=0, __get_cpuid_count(level, arg, &(a), &(b), &(c), &(d)) ? 0 : -1)
-/*
-  asm("cpuid" \
-      : "=a" (a), "=b" (b), "=c" (c), "=d" (d) \
-      : "0" (level), "2" (arg))
-*/
 #else
 #define cpuid(level, arg, a, b, c, d) \
 (a=b=c=d=0, -1)
